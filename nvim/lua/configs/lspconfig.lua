ace5f447dc6be1bb54f00db0ffb0a99949acb8fe
bastiangx <bastiangx@pm.me>
2024-09-05 13:44:20 +0100

    fixed lsp attach

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,102 +1,115 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+local lspconfig = require "lspconfig"
+
+local servers = {
+    "html",
+    "cssls",
+    -- "gopls",
+    -- "lua_lsp",
+    -- "rust_analyzer",
+    -- "ruff_lsp",
+    -- "basedpyright",
+}
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        on_attach = nvlsp.on_attach,
+        on_init = nvlsp.on_init,
+        capabilities = nvlsp.capabilities,
+    }
+end
+
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "lua_ls" }
+--
+--
+-- OLDER CONF
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
--- python
+-- -- python
 lspconfig.ruff_lsp.setup {
-  single_file_support = true,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" },
+    single_file_support = true,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "python" },
 }
-
+--
 lspconfig.basedpyright.setup {
-  settings = {
-    pyright = {
-      disableOrganizeImports = true,
+    settings = {
+        pyright = {
+            disableOrganizeImports = true,
+        },
+        python = {
+            analysis = {
+                ignore = { "*" },
+            },
+        },
     },
-    python = {
-      analysis = {
-        ignore = { "*" },
-      },
-    },
-  },
-  single_file_support = true,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" },
+    single_file_support = true,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "python" },
 }
-
--- Kotlin
+-- -- Kotlin
 lspconfig.kotlin_language_server.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "kotlin" },
-  root_dir = lspconfig.util.root_pattern("build.gradle.kts", "settings.gradle.kts", "gradlew", "pom.xml"),
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "kotlin" },
+    root_dir = lspconfig.util.root_pattern("build.gradle.kts", "settings.gradle.kts", "gradlew", "pom.xml"),
 }
-
--- rust_analyzer setup
+--
+-- -- rust_analyzer setup
 lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "rust", "rs" },
-  root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "rust", "rs" },
+    root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
 }
-
--- assembly lsp link -> https://github.com/bergercookie/asm-lsp
-lspconfig.asm_lsp.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  command = { "asm-lsp" },
-  filetypes = { "asm", "s", "S", "vmasm" },
-  -- root NEEDS .git to work properly
-  root_dir = lspconfig.util.find_git_ancestor,
-  docs = {
-    description = [[ 
-      https://github.com/bergercookie/asm-lsp
-
-      ls for GAS/GO assembly
-      ]],
-  },
+--
+-- -- golang
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+    cmd = { "gopls" },
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+        },
+    },
 }
-
--- java/jdtls options
+--
+-- -- java/jdtls options
 local jdtls_path = vim.fn.expand "~/.local/share/nvim/mason/packages/jdtls"
 lspconfig.jdtls.setup {
-  cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xms1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-jar",
-    jdtls_path .. "/plugins/org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar",
-    "-configuration",
-    jdtls_path .. "/config_linux",
-    "-data",
-    vim.fn.expand "~/.cache/jdtls-workspace",
-  },
-  root_dir = lspconfig.util.root_pattern(".git", "mvnw", "gradlew", "pom.xml", ".gitignore"),
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "java" },
+    cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        jdtls_path .. "/plugins/org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar",
+        "-configuration",
+        jdtls_path .. "/config_linux",
+        "-data",
+        vim.fn.expand "~/.cache/jdtls-workspace",
+    },
+    root_dir = lspconfig.util.root_pattern(".git", "mvnw", "gradlew", "pom.xml", ".gitignore"),
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "java" },
 }
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,280 +1,286 @@
 return {
-  -- NVCHAD Default plugins --
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+    -- NVCHAD Default plugins --
+    {
+        "stevearc/conform.nvim",
+        config = function()
+            require "configs.conform"
+        end,
+    },
 
-  -- cmp options overriding nvchad default
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      completion = {
-        keyword_length = 2,
-      },
-      windows = {
-        documentation = {
-          max_height = 12,
-          max_width = 32,
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+
+    -- cmp options overriding nvchad default
+    {
+        "hrsh7th/nvim-cmp",
+        opts = {
+            completion = {
+                keyword_length = 2,
+            },
+            windows = {
+                documentation = {
+                    max_height = 12,
+                    max_width = 32,
+                },
+            },
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "luasnip" },
+                { name = "rg" },
+            },
         },
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "luasnip" },
-        { name = "rg" },
-      },
     },
-  },
 
-  -- nvim-tree: some custom options
-  {
-    "kyazdani42/nvim-tree.lua",
-    opts = {
-      renderer = {
-        special_files = {
-          "Cargo.toml",
-          "main.rs",
-          "app.rs",
-          "init.rs",
-          "init.py",
-          "init.lua",
-          "app.py",
-          "main.py",
-          "readme.md",
-          "README.md",
+    -- nvim-tree: some custom options
+    {
+        "kyazdani42/nvim-tree.lua",
+        opts = {
+            renderer = {
+                special_files = {
+                    "Cargo.toml",
+                    "main.rs",
+                    "app.rs",
+                    "init.rs",
+                    "init.py",
+                    "init.lua",
+                    "app.py",
+                    "main.py",
+                    "readme.md",
+                    "README.md",
+                },
+            },
+            filters = {
+                dotfiles = false,
+            },
+            view = {
+                float = {
+                    enable = true,
+                    open_win_config = {
+                        width = 48,
+                        height = 28,
+                    },
+                },
+            },
         },
-      },
-      filters = {
-        dotfiles = false,
-      },
-      view = {
-        float = {
-          enable = true,
-          open_win_config = {
-            width = 48,
-            height = 28,
-          },
+    },
+
+    -- has custom list of langs to install
+    -- default opts from nvchad
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require "configs.treesitter"
+        end,
+        event = { "BufReadPost", "BufNewFile" },
+        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+        build = ":TSUpdate",
+    },
+
+    -- CUSTOM PLUGINS --
+
+    -- noice setup
+    {
+        "folke/noice.nvim",
+        depends = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
         },
-      },
+        config = function()
+            require "configs.noice"
+        end,
+        lazy = false,
     },
-  },
 
-  -- has custom list of langs to install
-  -- default opts from nvchad
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require "configs.treesitter"
-    end,
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-    build = ":TSUpdate",
-  },
-
-  -- CUSTOM PLUGINS --
-
-  -- noice setup
-  {
-    "folke/noice.nvim",
-    depends = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+    -- Leap dependency  - vim-repeat
+    "tpope/vim-repeat",
+    -- Leap / fast navigation
+    {
+        "ggandor/leap.nvim",
+        depends = "tpope/vim-repeat",
+        event = "VeryLazy",
     },
-    config = function()
-      require "configs.noice"
-    end,
-    lazy = false,
-  },
 
-  -- Leap dependency  - vim-repeat
-  "tpope/vim-repeat",
-  -- Leap / fast navigation
-  {
-    "ggandor/leap.nvim",
-    depends = "tpope/vim-repeat",
-    event = "VeryLazy",
-  },
+    -- copilot setup
+    {
+        "zbirenbaum/copilot.lua",
+        opts = function()
+            return require "configs.copilot"
+        end,
+        cmd = "Copilot",
+        event = "VeryLazy",
+    },
 
-  -- copilot setup
-  {
-    "zbirenbaum/copilot.lua",
-    opts = function()
-      return require "configs.copilot"
-    end,
-    cmd = "Copilot",
-    event = "VeryLazy",
-  },
+    -- arrow.nvim setup
+    {
+        "otavioschwanck/arrow.nvim",
+        opts = function()
+            return require "configs.arrow"
+        end,
+        lazy = false,
+    },
 
-  -- arrow.nvim setup
-  {
-    "otavioschwanck/arrow.nvim",
-    opts = function()
-      return require "configs.arrow"
-    end,
-    lazy = false,
-  },
+    -- oil.nvim setup
+    {
+        "stevearc/oil.nvim",
+        depends = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require "configs.oil"
+        end,
+        lazy = false,
+    },
 
-  -- oil.nvim setup
-  {
-    "stevearc/oil.nvim",
-    depends = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require "configs.oil"
-    end,
-    lazy = false,
-  },
+    -- java lsp setup
+    {
+        "nvim-java/nvim-java",
+        config = function()
+            require("nvim-java").setup()
+        end,
+        ft = "java",
+    },
 
-  -- java lsp setup
-  {
-    "nvim-java/nvim-java",
-    config = function()
-      require("nvim-java").setup()
-    end,
-    ft = "java",
-  },
-
-  -- rust tools setup
-  {
-    "simrat39/rust-tools.nvim",
-    opts = {
-      tools = {
-        max_len_align = true,
-        max_len_align_padding = 2,
-        hover_actions = {
-          auto_focus = true,
+    -- rust tools setup
+    {
+        "simrat39/rust-tools.nvim",
+        opts = {
+            tools = {
+                max_len_align = true,
+                max_len_align_padding = 2,
+                hover_actions = {
+                    auto_focus = true,
+                },
+            },
         },
-      },
+        config = function(_, opts)
+            require("rust-tools").setup(opts)
+        end,
+        ft = "rust",
+        event = "BufReadPost *.rs",
+        depends = "neovim/nvim-lspconfig",
     },
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
-    end,
-    ft = "rust",
-    event = "BufReadPost *.rs",
-    depends = "neovim/nvim-lspconfig",
-  },
 
-  -- rust crates setup
-  {
-    "saecki/crates.nvim",
-    ft = { "toml" },
-    event = { "BufReadPost Cargo.toml" },
-    config = function()
-      local crates = require "crates"
-      crates.setup()
-      crates.show()
-    end,
-  },
-
-  -- todo comments, highlights and icons
-  {
-    "folke/todo-comments.nvim",
-    depends = "nvim-lua/plenary.nvim",
-    opts = function()
-      return require "configs.comments"
-    end,
-    event = "BufReadPost",
-  },
-
-  -- Renamer
-  {
-    "filipdutescu/renamer.nvim",
-    depends = "nvim-lua/plenary.nvim",
-    config = function()
-      require "configs.renamer"
-    end,
-    event = "LspAttach",
-  },
-
-  -- Mini indentline
-  {
-    "echasnovski/mini.nvim",
-    opts = {
-      draw = {
-        delay = 25,
-      },
-      symbol = "▏",
+    -- rust crates setup
+    {
+        "saecki/crates.nvim",
+        ft = { "toml" },
+        event = { "BufReadPost Cargo.toml" },
+        config = function()
+            local crates = require "crates"
+            crates.setup()
+            crates.show()
+        end,
     },
-    config = function(_, opts)
-      require("mini.indentscope").setup(opts)
-    end,
-    event = "BufRead",
-  },
 
-  -- LSP guide/help/docs popup
-  {
-    "LukasPietzschmann/boo.nvim",
-    config = function()
-      require "configs.boo"
-    end,
-    event = "LspAttach",
-  },
+    -- todo comments, highlights and icons
+    {
+        "folke/todo-comments.nvim",
+        depends = "nvim-lua/plenary.nvim",
+        opts = function()
+            return require "configs.comments"
+        end,
+        event = "BufReadPost",
+    },
 
-  -- word/textObject case switcher
-  {
-    "gregorias/coerce.nvim",
-    config = function()
-      require "configs.coerce"
-    end,
-    depends = "folke/which-key.nvim",
-    event = "VeryLazy",
-  },
+    -- Renamer
+    {
+        "filipdutescu/renamer.nvim",
+        depends = "nvim-lua/plenary.nvim",
+        config = function()
+            require "configs.renamer"
+        end,
+        event = "LspAttach",
+    },
 
-  -- center the buffer
-  {
-    "shortcuts/no-neck-pain.nvim",
-    config = function()
-      require "configs.no-neck-pain"
-    end,
-    event = "VeryLazy",
-  },
+    -- Mini indentline
+    {
+        "echasnovski/mini.nvim",
+        opts = {
+            draw = {
+                delay = 25,
+            },
+            symbol = "▏",
+        },
+        config = function(_, opts)
+            require("mini.indentscope").setup(opts)
+        end,
+        event = "BufRead",
+    },
 
-  -- startup - spcaeport for project finding
-  {
-    "CWood-sdf/spaceport.nvim",
-    opts = function()
-      require "configs.spaceport"
-    end,
-    cmd = "Spaceport",
-    lazy = false,
-  },
+    -- LSP guide/help/docs popup
+    {
+        "LukasPietzschmann/boo.nvim",
+        config = function()
+            require "configs.boo"
+        end,
+        event = "LspAttach",
+    },
 
-  -- obsidian.nvim seup
-  {
-    "epwalsh/obsidian.nvim",
-    depends = "nvim-lua/plenary.nvim",
-    ft = { "md", "markdown" },
-    event = "BufReadPost *.md",
-    config = function()
-      require "configs.obsidian"
-    end,
-  },
+    -- word/textObject case switcher
+    {
+        "gregorias/coerce.nvim",
+        config = function()
+            require "configs.coerce"
+        end,
+        depends = "folke/which-key.nvim",
+        event = "VeryLazy",
+    },
 
-  {
-    -- cmp -rg: ripgrep source for nvim-cmp
-    "lukas-reineke/cmp-rg",
-    depends = "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-  },
+    -- center the buffer
+    {
+        "shortcuts/no-neck-pain.nvim",
+        config = function()
+            require "configs.no-neck-pain"
+        end,
+        event = "VeryLazy",
+    },
 
-  --- Disabled PLUGINS ---
-  { "NvChad/nvim-colorizer.lua", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "gen740/SmoothCursor.nvim", enabled = false },
+    -- startup - spcaeport for project finding
+    {
+        "CWood-sdf/spaceport.nvim",
+        opts = function()
+            require "configs.spaceport"
+        end,
+        cmd = "Spaceport",
+        lazy = false,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
+    -- obsidian.nvim seup
+    {
+        "epwalsh/obsidian.nvim",
+        depends = "nvim-lua/plenary.nvim",
+        ft = { "md", "markdown" },
+        event = "BufReadPost *.md",
+        config = function()
+            require "configs.obsidian"
+        end,
+    },
+
+    {
+        -- cmp -rg: ripgrep source for nvim-cmp
+        "lukas-reineke/cmp-rg",
+        depends = "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+    },
+
+    --- Disabled PLUGINS ---
+    { "NvChad/nvim-colorizer.lua", enabled = false },
+    { "lukas-reineke/indent-blankline.nvim", enabled = false },
+
+    -- These are some examples, uncomment them if you want to see them work!
+    --
+    -- {
+    -- 	"williamboman/mason.nvim",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"lua-language-server", "stylua",
+    -- 			"html-lsp", "css-lsp" , "prettier"
+    -- 		},
+    -- 	},
+    -- },
+    --
 }
