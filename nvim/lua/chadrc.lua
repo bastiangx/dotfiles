3e81e832ee822b21fdeb7de8dc85ab7ebc4252fa
bastiangx <bastiangx@pm.me>
2024-10-02 18:18:42 +0100

    update:nvchad3.0

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,37 +1,138 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-  -- NVChad stuff
-  theme = "tundra",
-  theme_toggle = { "tundra", "mountain" },
-  transparency = true,
+M.base46 = {
+    theme = "tundra",
+    theme_toggle = { "tundra", "mountain" },
+    transparency = true,
 
-  -- highlights
-  hl_override = {
-    Comment = { italic = true, fg = "#3A5F76" },
-    ["@comment"] = { link = "Comment" },
-    ["@comment.rust"] = { link = "Comment" },
-    ["@comment.python"] = { link = "Comment" },
-    ["@comment.java"] = { link = "Comment" },
-    ["@comment.markdown"] = { link = "Comment" },
-    ["@comment.txt"] = { link = "Comment" },
-    ["@comment.css"] = { link = "Comment" },
-    ["@comment.toml"] = { link = "Comment" },
+    -- highlights
+    hl_override = {
+        Comment = { italic = true, fg = "#3A5F76" },
+        ["@comment"] = { link = "Comment" },
+        ["@comment.rust"] = { link = "Comment" },
+        ["@comment.python"] = { link = "Comment" },
+        ["@comment.java"] = { link = "Comment" },
+        ["@comment.go"] = { link = "Comment" },
+        ["@comment.markdown"] = { link = "Comment" },
+        ["@comment.txt"] = { link = "Comment" },
+        ["@comment.css"] = { link = "Comment" },
+        ["@comment.toml"] = { link = "Comment" },
 
-    -- Types
-    ["rustTypedef"] = { italic = true },
-    ["type"] = { italic = true },
-    ["@type"] = { italic = true },
-    ["Type"] = { italic = true },
+        -- Types
+        ["rustTypedef"] = { italic = true },
+        ["type"] = { italic = true },
+        ["@type"] = { italic = true },
+        ["Type"] = { italic = true },
 
-    -- Delimiter for miniIndent lines
-    ["Delimiter"] = { fg = "#4c566a" },
-    ["@delimiter"] = { link = "Delimiter" },
-  },
+        -- Delimiter for miniIndent lines
+        ["Delimiter"] = { fg = "#4c566a" },
+        ["@delimiter"] = { link = "Delimiter" },
+    },
+
+    ui = {
+        cmp = {
+            icons_left = false, -- only for non-atom styles!
+            lspkind_text = true,
+            style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+            format_colors = {
+                tailwind = false, -- will work for css lsp too
+                icon = "󱓻",
+            },
+        },
+
+        telescope = { style = "borderless" }, -- borderless / bordered
+
+        statusline = {
+            theme = "default", -- default/vscode/vscode_colored/minimal
+            -- default/round/block/arrow separators work only for default statusline theme
+            -- round and block will work for minimal theme only
+            separator_style = "default",
+            order = nil,
+            modules = nil,
+        },
+
+        -- lazyload it when there are 1+ buffers
+        tabufline = {
+            enabled = true,
+            lazyload = true,
+            order = { "treeOffset", "buffers", "tabs", "btns" },
+            modules = nil,
+        },
+    },
+
+    nvdash = {
+        load_on_startup = true,
+        header = {
+            "                            ",
+            "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+            "   ▄▀███▄     ▄██ █████▀    ",
+            "   ██▄▀███▄   ███           ",
+            "   ███  ▀███▄ ███           ",
+            "   ███    ▀██ ███           ",
+            "   ███      ▀ ███           ",
+            "   ▀██ █████▄▀█▀▄██████▄    ",
+            "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+            "                            ",
+            "     Powered By  eovim    ",
+            "                            ",
+        },
+
+        buttons = {
+            { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+            { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+            { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
+            { txt = "󱥚  Themes", keys = "Spc t h", cmd = "Telescope themes" },
+            { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+
+            { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+            {
+                txt = function()
+                    local stats = require("lazy").stats()
+                    local ms = math.floor(stats.startuptime) .. " ms"
+                    return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+                end,
+                hl = "NvDashLazy",
+                no_gap = true,
+            },
+
+            { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+        },
+    },
+
+    term = {
+        winopts = { number = false, relativenumber = false },
+        sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+        float = {
+            relative = "editor",
+            row = 0.3,
+            col = 0.25,
+            width = 0.5,
+            height = 0.4,
+            border = "single",
+        },
+    },
+
+    lsp = { signature = true },
+
+    cheatsheet = {
+        theme = "grid", -- simple/grid
+        excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
+    },
+
+    mason = { pkgs = {} },
+
+    colorify = {
+        enabled = true,
+        mode = "virtual", -- fg, bg, virtual
+        virt_text = "󱓻 ",
+        highlight = { hex = true, lspvars = true },
+    },
 }
 
 return M
