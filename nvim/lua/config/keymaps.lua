067711d3ea69bc5cd29722c626c32b681c2cd133
bastiangx <bastiangx@pm.me>
2025-09-20 19:09:35 +0100

    keymaps: added select all lines, copy and paste into whole file

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,7 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = false })
+
+vim.keymap.set("n", "<leader>aa", "ggVG", { desc = "Select whole file" })
+vim.keymap.set("n", "<leader>ac", "ggVGy", { desc = "Yank whole file" })
+vim.keymap.set("n", "<leader>ap", "ggVGp", { desc = "Replace whole file with clipboard" })
